7fed512ff26ffe6427d13151d1adb0ecf3a29c0e
unknown <joseph.ryu.pogue@emory.edu>
2021-03-01 22:27:47 +0900

    testing architecture

--- frontend-createmory/src/App.js
+++ frontend-createmory/src/App.js
@@ -1,13 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from 'ejs';
+
+function NavBar(){
+  return(
+    <>
+    </>
+  );
+}
+
+function HomePage() {
+  return(
+    <h1> HomePage</h1>
+  );
+}
+function AboutUs() {
+  return(
+    <h1>AboutUs</h1>
+  );
+}
+
+function ContactUs() {
+  return(
+    <h1>ContactUs</h1>
+  );
+}
+
+function Footer(){
+  return(
+    <>
+      <h1>This is the footer</h1>
+    </>
+  );
+}
 
 function App() {
   return (
     <div className="App">
 
       <h1>CREATEmory</h1>
+      <HomePage/>
+      <AboutUs/>
+      <ContactUs/>
+      <Footer/>
 
     </div>
+  /* 
+    <Router>
+      <Navbar/>
+    </Router>
+    */
   );
 }
 
