fdd7a1838ace279a0ca4abc60642baf28203c9b6
will <sopwill3@gmail.com>
2021-03-06 22:05:06 -0500

    added router and empty components

--- frontend-createmory/src/App.js
+++ frontend-createmory/src/App.js
@@ -1,55 +1,40 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { render } from 'ejs';
 
-function NavBar(){
-  return(
-    <>
-    </>
-  );
-}
-
-function HomePage() {
-  return(
-    <h1> HomePage</h1>
-  );
-}
-function AboutUs() {
-  return(
-    <h1>AboutUs</h1>
-  );
-}
-
-function ContactUs() {
-  return(
-    <h1>ContactUs</h1>
-  );
-}
-
-function Footer(){
-  return(
-    <>
-      <h1>This is the footer</h1>
-    </>
-  );
-}
+// Components
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from "react-router-dom";
+import LayoutOne from './components/Layout/Layout';
+import AboutPage from './components/AboutPage/AboutPage'
+import BlogPage from './components/BlogPage/BlogPage'
+import HomePage from './components/HomePage/HomePage'
+import PortfolioPage from './components/PortfolioPage/PortfolioPage'
+import ContactPage from './components/ContactPage/ContactPage'
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
+      <Switch>
+        <RouteWrapper path="/" component={HomePage} layout={LayoutOne} />
+        <RouteWrapper path="/about" component={AboutPage} layout={LayoutOne} />
+        <RouteWrapper path="/blog" component={BlogPage} layout={LayoutOne} />
+        <RouteWrapper path="/portfolio" component={PortfolioPage} layout={LayoutOne} />
+        <RouteWrapper path="/contact" component={ContactPage} layout={LayoutOne} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
-      <h1>CREATEmory</h1>
-      <HomePage/>
-      <AboutUs/>
-      <ContactUs/>
-      <Footer/>
-
-    </div>
-  /* 
-    <Router>
-      <Navbar/>
-    </Router>
-    */
+const RouteWrapper = ({component: Component, layout: Layout, path}) => {
+  return (
+    <Route path={path} >
+      <Layout >
+        <Component />
+      </Layout>
+    </Route>
   );
 }
 
---
+++ frontend-createmory/src/components/Layout/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// Components
+import { Grid, Container } from '@material-ui/core';
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Layout = ({ children }) => {
+  <div>
+    {children}
+  </div>
+}
+
+
+export default Layout;
