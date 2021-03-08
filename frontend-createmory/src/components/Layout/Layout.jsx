738704ee757099a02bff2861de1982dd345d4841
will <sopwill3@gmail.com>
2021-03-07 19:29:50 -0500

    initial stuff

--- frontend-createmory/src/components/Layout/Layout.jsx
+++ frontend-createmory/src/components/Layout/Layout.jsx
@@ -1,19 +1,36 @@
 import React from 'react';
 
 // Components
-import { Grid, Container } from '@material-ui/core';
+import { Grid, Box, Container } from '@material-ui/core';
 import Navbar from './Navbar'
 import Footer from './Footer'
 
+// Styles
+import styles from './styles/layout.module.css'
+
 const Layout = ({ children }) => {
   return(
-    <div>
+    <Grid container>
       <Navbar/>
-      <Container>{children}</Container>
-      <Container><Footer/></Container>
-    </div>
+      <Container>
+        {children}
+      </Container>
+      <Footer/>
+    </Grid>
   )
 }
 
 
 export default Layout;
+
+
+
+
+
+
+
+
+
+
+
+
--- frontend-createmory/src/components/Layout/Navbar.jsx
+++ frontend-createmory/src/components/Layout/Navbar.jsx
@@ -1,12 +1,79 @@
 import React from 'react';
 
 // Components
-import { Grid } from '@material-ui/core';
+import {
+  Button,
+  Grid,
+  Typography,
+  MenuItem,
+  Select,
+  InputLabel,
+  FormControl,
+  Box,
+} from '@material-ui/core';
 import {Link} from 'react-router-dom'
+
+// Styles
+import styles from './styles/navbar.module.css'
 
 const Navbar = (props) => {
   return(
-    <Grid container spacing={3}>
+    <Grid 
+      className={styles.navbarBox}  
+      container 
+      item 
+      sm={12}
+      spacing={2}
+      justify="center"
+    >
+      <Grid 
+        item
+        container 
+        xs={2}
+      >
+        <Link to="/">Home</Link>
+      </Grid>
+      <Grid item xs={2}>
+        <Link to="/about">About</Link>
+      </Grid>
+      <Grid item xs={2}>
+        <Link to="/contact">Contact</Link>      
+      </Grid>
+      <Grid item xs={2}>
+      <Link to="/blog">Blog</Link>
+      </Grid>
+      <Grid item xs={2}>
+        <Link to="/portfolio">Portfolio</Link>
+      </Grid>
+    </Grid>
+  )
+}
+
+
+export default Navbar;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    <Grid className={styles.navbarBox} container spacing={3}>
       <Grid item xs={2}>
           <Link to="/">Home</Link>
         </Grid>
@@ -23,8 +90,4 @@ const Navbar = (props) => {
           <Link to="/portfolio">Portfolio</Link>
         </Grid>
     </Grid>
-  )
-}
-
-
-export default Navbar;
+*/
