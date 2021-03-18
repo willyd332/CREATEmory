3d1ca6a7c27e267c3ebd5b2a0b88c3666a75ced4
unknown <joseph.ryu.pogue@emory.edu>
2021-03-18 21:02:09 +0900

    tried adding a simple footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,13 +1,73 @@
 import React from 'react';
-
+import {
+  AppBar,
+  Container,
+  Typography,
+  Toolbar,
+  Grid
+} from '@material-ui/core'; 
 // Components
-
+const styles = {
+  footer: {
+  }
+}
 const Footer = (props) => {
-  return(
+  // return(
+  //   <div>
+  //     <h1>Footer</h1>
+  //   </div>
+  // )
+  return (
+    // <AppBar position="static" color="primary">
+    //   <Container maxWidth="xl">
+    //     <Toolbar>
+    //       <Typography variant="body1" color="inherit">
+    //         &copy; {new Date().getFullYear()} CREATEmory
+    //       </Typography>
+    //     </Toolbar>
+    //   </Container>
+    // </AppBar>
+    
     <div>
-      <h1>Footer</h1>
+      <AppBar position="static" color="white" style={styles.footer}>
+        
+        <Toolbar>
+          <Grid container justify={"center"} spacing={2}>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography align={"center"}>
+                About
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography align={"center"}>
+                Contact
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography align={"center"}>
+                Blog
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Typography align={"center"}>
+                Portfolio
+              </Typography>
+            </Grid>
+          </Grid>
+          
+        </Toolbar>
+        <Toolbar>
+        <Grid container item >
+              <Typography align={"left"}>
+                &copy;{new Date().getFullYear()} CREATEmory | All rights reserved | Terms | Policy
+              </Typography>
+            </Grid>
+        </Toolbar>
+      
+      </AppBar>
+      
     </div>
-  )
+)
 }
 
 
